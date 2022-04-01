03eab200cffbc166e04fb7aa1d14bb416762088d
msmelissacz21 <msmelissacz21@gmail.com>
2022-03-31 20:49:21 -0500

    First commit

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,24 +23,46 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(stop())
+            },time)
+        })
+    
     }
 
-    function walkNorth() {
+    function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(stop())
+
+            }, time)
+        })
     }
 
-    function walkWest() {
+    function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(stop())
+            }, time)
+        }) 
     }
 
-    function walkSouth() {
+    function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(stop())
+            }, time)
+        })
     }
 
     function stop() {
